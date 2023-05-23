cd5110040ac3eeb50752615e766efe9b4ab6ab9a
vickats <victoriaktsuha@outlook.com>
2023-05-23 15:47:42 -0300

    Modulo 4 - Aula 2
    02. Variáveis | Operadores

--- project/JS/script.js
+++ project/JS/script.js
@@ -1,4 +1,40 @@
 "use strict";
-console.log("Hello World");
+console.log("Hello World!");
 // console.error();
 // console.warn();
+
+/////////////////
+// Variaveis
+
+let idade = 26;
+console.log(idade);
+
+// Tipos de variaveis
+// não se usa mais 'var'
+
+idade = 30;
+let numero = 30;
+let altura = 1.77;
+let nome = "nome";
+let eValido = true;
+let outra;
+let nula = null;
+
+const constante = 100;
+
+console.log(
+  idade,
+  typeof idade,
+  numero,
+  typeof numero,
+  altura,
+  typeof altura,
+  nome,
+  typeof nome,
+  eValido,
+  typeof eValido,
+  outra,
+  typeof outra,
+  nula,
+  typeof nula
+);
