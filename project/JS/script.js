8fd9d1222942b4e945a5c9f37517492b6a2b468a
vickats <victoriaktsuha@outlook.com>
2023-05-23 16:18:24 -0300

    Modulo 4 - Aula 4
    04. Operadores Booleanos

--- project/JS/script.js
+++ project/JS/script.js
@@ -73,3 +73,43 @@ console.log("Resto da Divisão =", numero1 % numero2);
 // Biblioteca Math
 console.log(Math.PI); /*valor de PI*/
 console.log(Math.sqrt(16)); /* calcula a √ */
+
+/////////////////
+// Operadores booleanos
+
+// Igualdade:           == (ou ===)
+// Desigualdade:        != (ou !==)
+// Maior que:           >
+// Maior ou igual:      >=
+// Menor que:           <
+// Menor ou igual:      <=
+
+/* '=' atribuição / '== ou ===' equiparação */
+const number = 10;
+console.log(number > 20); //false
+console.log(number == 10); //true
+
+console.log(number == "10"); //true - compara conteudo
+console.log(number === "10"); //false - compara conteudo e tipo
+/* boa prática utilizar '===' / '!==' */
+
+// Conjunções lógicas
+
+// AND => &&
+
+let idade2 = 26;
+let tenhoCNH = true;
+const possoDirigir = idade2 >= 18 && tenhoCNH === true;
+console.log("Posso dirigir ?", possoDirigir);
+
+// OR => ||
+
+let idade3 = 40;
+
+const votoFacultativo = idade < 18 || idade >= 70;
+console.log("Posso não votar ?", votoFacultativo);
+
+// NOT => !
+
+const estouGostandoDoCurso = false;
+console.log(!estouGostandoDoCurso);
