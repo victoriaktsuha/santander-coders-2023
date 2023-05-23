118282b012a80364d2aca5d711fe6284e0fcfbcf
vickats <victoriaktsuha@outlook.com>
2023-05-23 16:04:47 -0300

    Modulo 4 - Aula 3
    03. Operadores Aritméticos

--- project/JS/script.js
+++ project/JS/script.js
@@ -11,6 +11,9 @@ console.log(idade);
 
 // Tipos de variaveis
 // não se usa mais 'var'
+// não coloque numero no inicio da variável
+// utilize camelCase
+// não utilize palavras-chave reservadas apenas ao JS como log,splice, concat, etc...
 
 idade = 30;
 let numero = 30;
@@ -38,3 +41,35 @@ console.log(
   nula,
   typeof nula
 );
+
+/////////////////
+// Operadores aritmeticos
+
+let numero1 = 20;
+let numero2 = 2;
+
+// Adição:            +
+// Subtração:         -
+// Multiplicação:     *
+// Divisão:           /
+// Exponenciação:     **
+// Resto da Divisão:  %
+
+const soma = numero1 + numero2;
+console.log(`Soma = ${numero1 + numero2}`);
+console.log(`Soma = ${soma}`);
+console.log("Soma =", soma);
+console.log("Subtração =", numero1 - numero2);
+console.log("Multiplicação =", numero1 * numero2);
+console.log("Divisão =", numero1 / numero2);
+console.log("Exponenciação =", numero1 ** numero2);
+console.log("Resto da Divisão =", numero1 % numero2);
+
+// Precedencia de operadores
+
+// Ratificação => Exponenciação => Multiplicação => Adição/Subtração
+// Para dar prioridade a alguma opreção, englobe com ()
+
+// Biblioteca Math
+console.log(Math.PI); /*valor de PI*/
+console.log(Math.sqrt(16)); /* calcula a √ */
