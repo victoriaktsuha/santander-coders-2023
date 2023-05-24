9eba231304e48777f06218b000213c655805bd34
vickats <victoriaktsuha@outlook.com>
2023-05-24 13:28:53 -0300

    Modulo 5 - Aula 5
    05. Renderização condicional

--- project/React/meu-projeto-react/src/components/MeuContador.jsx
+++ project/React/meu-projeto-react/src/components/MeuContador.jsx
@@ -3,18 +3,32 @@ import { useState } from "react"; /*Estado*/
 export default function MeuContador() {
   console.log("Renderizando meu contador");
   const [contador, setContador] = useState(0); /*Estado*/
-  /* 'contador' = variavel; 'setContador' = função de 'iteração' - que começa em zero (useState(0)) - useStates é utilizado para que o componente inteiro seja atualizado, especificamente {contador}, quando o EVENTO onClick={iterar} é acionado*/
+  /* 'contador' = variavel de estado; 'setContador' = função de 'iteração' - que começa em zero (useState(0)) - useStates é utilizado para que o componente inteiro seja atualizado, especificamente {contador}, quando o EVENTO onClick={iterar} é acionado*/
 
   function iterar() {
     setContador(contador + 1);
     /* funciona como 'contador++' */
   }
+  function remover() {
+    setContador(contador - 1);
+    /* funciona como 'contador++' */
+  }
+  if (contador > 5) {
+    return (
+      <div>
+        <h1>Valor muito alto!</h1>
+        <button onClick={remover}>Diminuir</button>
+      </div>
+    );
+  }
 
   return (
     <div>
-      <h1>Meu Contador</h1>
-      <h3>{contador}</h3>
+      <h1>Meu Contador: {contador}</h1>
+      {/* {contador > 9 ? <h1>Valor muito grande</h1> : null} */}
       <button onClick={iterar}>Iterar</button>
+      <button onClick={remover}>Diminuir</button>
     </div>
   );
 }
+// {contador > 9 ? <h1>Valor muito grande</h1> : null} => renderização condicional (operador ternário)
